600df4c039b04eb9f29bde384a274646f557b85f
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-03-24 02:24:21 -0300

    Atualização do projeto

--- DevReviews/DevReviews_API/Models/ProductDetailsViewModel.cs
+++ DevReviews/DevReviews_API/Models/ProductDetailsViewModel.cs
@@ -19,4 +19,22 @@
         public DateTime RegisteredAt { get; private set; }
         public List<ProductReviewViewModel> Reviews { get; private set; }
     }
+
+    public class ProductReviewViewModel
+    {
+        public ProductReviewViewModel(int id, string author, int rating, string comments, DateTime registeredAt)
+        {
+            Id = id;
+            Author = author;
+            Rating = rating;
+            Comments = comments;
+            RegisteredAt = registeredAt;
+        }
+
+        public int Id { get; private set; }
+        public string Author { get; private set; }
+        public int Rating { get; private set; }
+        public string Comments { get; private set; }
+        public DateTime RegisteredAt { get; private set; }
+    }
 }
